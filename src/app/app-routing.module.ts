4938c8996f5a33970a0266308556dd176eab22ef
Lilia Said <yazminsaid@gmail.com>
2020-05-24 19:37:02 -0300

    avance punto 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,14 @@ import { Punto1Component} from './components/punto1/punto1.component';
 
 import { Punto3Component} from './components/punto3/punto3.component';
 import { Punto4Component} from './components/punto4/punto4.component';
-import { Punto5Component} from './components/punto5/punto5.component';
 import { HeaderComponent} from './components/header/header.component';
 import { HomeComponent} from './components/home/home.component';
 import { FooterComponent} from './components/footer/footer.component';
 import { DatatablesClientsComponent} from "./components/datatables-clients/datatables-clients.component";
 const routes: Routes = [
-  {path: 'punto1', component:Punto1Component},
-  
+  {path: 'punto1', component:Punto1Component},  
   {path: 'punto3', component:Punto3Component},
   {path: 'punto4', component:Punto4Component},
-  {path: 'punto5', component:Punto5Component},
   {path: 'header', component:HeaderComponent},
   {path: 'home', component:HomeComponent},
   {path: 'footer', component:FooterComponent},
